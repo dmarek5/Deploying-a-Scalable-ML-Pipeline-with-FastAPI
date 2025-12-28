d5cc6eda0efebd1879b20856089955bc7d04f53f
Dulce M <dmare13@wgu.edu>
2025-12-28 00:06:14 +0000

    all files

--- model_card_template.md
+++ model_card_template.md
@@ -2,17 +2,26 @@
 
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
-## Model Details
+## Model Overview 
+This is a logistic regression classifier that predicts individual income being more or less than $50k based on demographics. Such as age, education, occupation and hours worked per week.
 
-## Intended Use
+## Inteded Use
+This model can be used by organizations for predicting the income bracket of individual demographics. It could be used for studies with income disparities across different population groups.
 
-## Training Data
+## Dataset Overview
+The model was trained using the Census Income Dataset. The dataset contains demographic information such as age, working class, ocupation, hours worked, and education.
 
 ## Evaluation Data
+The model was evaluated using a test set from the same Census Income Dataset. It includes similar democraphic features as the training set.
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+-**Precision**: 0.7306
+-**Recall**: 0.5646
+-**F1 Score**: 0.6370
+Metrics were selected to evaluate the model's performance identifying differences between individuals that earn more or less than $50K 
 
 ## Ethical Considerations
+Potential biases from data inherited, such as ones based on **race** or **gender**. It may influence the model's predictions.
 
 ## Caveats and Recommendations
+The predictions will be bias if the training data is also. Retrain model with more diverse data.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,53 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from ml.model import train_model, compute_model_metrics, inference
+from ml.data import process_data
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
 
+# See if model returns an instance of LogisticRegression
+def test_train_model():
+    """
+    # Training model test to validate the type of processed data
+    """
+    X_train = np.random.rand(100,30)
+    y_train = np.random.randint(2,size=100)
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, LogisticRegression), f"Expected LogisticRegression, but got {type(model)}"
+
+
+def test_compute_model_metrics():
+    y_true=np.array([1,0,0,1])
+    y_pred=np.array([1,0,1,1])
+    
+    precision, recall, fbeta = compute_model_metrics(y_true,y_pred)
+
+    assert isinstance(precision, float),f"Expected float, but got {type(precision)}"
+    assert isinstance(recall,float), f"Expected float, but got {type(recall)}"
+    assert isinstance(fbeta,float), f"Expected float, but got {type(fbeta)}"
+    
+    
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+def test_process_data():
+    data= pd.DataFrame({
+    'age': [25,30,40,22],
+    'workclass':['Private','Self-emp-inc', 'Private','Local-gov'],
+    'education':['Bachelor', 'HS-grad', 'Masters', 'PhD'],
+    'hours-per-week': [40,50,60,30],
+    'salary': ['<=50K','<=50K', '>50K', '>50K']
+    })
+    cat_features= ['workclass', 'education']
+    label='salary'
+
+    X_processed, y_processed, encoder, lb = process_data(
+        data, categorical_features=cat_features, label=label, training=True
+    )
+    assert X_processed.shape[1] > len(cat_features), f"Expected more features after encoding, but got {X_processed.shape[1]}"
+    assert X_processed.shape[0] == len(y_processed), f"X and y should have the same number of rows, but got {X_processed.shape[0]} and {len(y_processed)}"
+
+    
